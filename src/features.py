7163273b8fef9d075126560b6b063c58897e0b52
theoapp <theoappourchaux62@gmail.com>
2026-02-20 20:47:12 +0100

    feat: start pipeline, extracting features

---
+++ src/features.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.signal import welch
+
+class PowerBandExtractor(BaseEstimator, TransformerMixin):
+    """
+    Extract the average power in specific frequency bands (e.g., Mu and Beta) from EEG epochs using Fourier analysis.
+    """
+    def __init__(self, sfreq=160.0, bands=None):
+        self.sfreq = sfreq
+        
+        if bands is None:
+            self.bands = {
+                'mu': (8, 12),
+                'beta': (13, 30)
+            }
+        else:
+            self.bands = bands
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        """
+        Take a 3D array of shape (n_epochs, n_channels, n_times) and return a 2D array of shape (n_epochs, n_features) where n_features = n_channels * n_bands.
+        Each feature corresponds to the average power in a specific frequency band for a specific channel.
+        """
+        n_epochs, n_channels, n_times = X.shape
+        n_bands = len(self.bands)
+        n_features = n_channels * n_bands
+        X_features = np.zeros((n_epochs, n_features))
+        
+        print(f"Extraction des puissances spectrales (Fourier) pour {n_epochs} époques...")
+        
+        for i in range(n_epochs):
+            for j in range(n_channels):
+                freqs, psd = welch(X[i, j, :], fs=self.sfreq, nperseg=int(self.sfreq))
+                
+                feature_idx = j * n_bands
+                for band_name, (fmin, fmax) in self.bands.items():
+                    idx_band = np.logical_and(freqs >= fmin, freqs <= fmax)
+                    band_power = np.mean(psd[idx_band])
+                    X_features[i, feature_idx] = band_power
+                    feature_idx += 1
+                    
+        return X_features
--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ from preprocessing import preprocessing
 import matplotlib
 matplotlib.use('qtagg')
 import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from features import PowerBandExtractor
 
 def main():
     parser = argparse.ArgumentParser(description="BCI Pipeline using Machine Learning.")
     parser.add_argument("subject", type=int, help="Subject ID (e.g., 1 for S001)")
     parser.add_argument("runs", type=str, nargs='+', help="List of runs to process (e.g., 4 8 12) or 'all'")
-    
     parser.add_argument("--path", type=str, default=None, help="Base path to the dataset")
     parser.add_argument("--plot", action="store_true", help="Visualize raw and filtered data for the first run")
     
@@ -16,21 +18,32 @@ def main():
     
     if 'all' in [r.lower() for r in args.runs]:
         target_runs = list(range(1, 15))
-        print("Option 'all' détectée : traitement des 14 runs.")
     else:
         try:
             target_runs = [int(r) for r in args.runs]
         except ValueError:
-            print("Erreur : Les runs doivent être des nombres entiers ou 'all'.")
+            print("Error : Invalid run numbers.")
             return
             
     X, y = preprocessing(subject_id=args.subject, runs=target_runs, base_path=args.path, plot=args.plot)
     
     if X is not None:
         print("\n--- PREPROCESSING COMPLETE ---")
-        print(f"Final X shape : {X.shape}")
-        print(f"Final y shape : {y.shape}")
+        print(f"Shape of X (3D) : {X.shape}")
+        print(f"Shape of y      : {y.shape}")
         
+        print("\n--- 2. CREATION OF THE PIPELINE ---")
+        
+        pipeline = Pipeline([
+            ('feature_extraction', PowerBandExtractor()),
+            # ('dimensionality_reduction', ...), #Todo: : add my CSP here
+            ('classifier', SVC(kernel='linear'))
+        ])
+        
+        extractor = pipeline.named_steps['feature_extraction']
+        X_2D = extractor.fit_transform(X)
+        
+        print(f"Shape of X after Fourier (2D) : {X_2D.shape}") 
 
 if __name__ == "__main__":
     main()
